b7be4b5b2e076366ee9005d81d9731bef17d1ce2
uraw <uraw@urgy.net>
2014-10-22 19:28:17 +0900

    Changes added by Custom automatically

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,3 +13,15 @@
 ;;; http://tech.kayac.com/archive/divide-dot-emacs.html
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
