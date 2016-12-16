0cc154b0a9e664beac8c3978bfd21f707bca6f91
uraw <uraw@urgy.net>
2016-12-17 01:59:56 +0900

    jaspace.el changes

--- emacs/.emacs.d/inits/02_appearance.el
+++ emacs/.emacs.d/inits/02_appearance.el
@@ -33,53 +33,63 @@
 
 ;;; jaspace.el
 ;;; http://homepage3.nifty.com/satomii/software/elisp.ja.html
-(require 'jaspace)
+;(require 'jaspace)
+;
+;;; タブ, 全角スペース、改行直前の半角スペースを表示する
+;(when (require 'jaspace nil t)
+;  (when (boundp 'jaspace-modes)
+;    (setq jaspace-modes (append jaspace-modes
+;(list 'php-mode
+;				      'yaml-mode
+;				      'javascript-mode
+;				      'ruby-mode
+;				      'text-mode
+;				      'fundamental-mode))))
+;  (when (boundp 'jaspace-alternate-jaspace-string)
+;    (setq jaspace-alternate-jaspace-string "□"))
+;;  (when (boundp 'jaspace-highlight-tabs)
+;;    (setq jaspace-highlight-tabs ?^))
+;  (add-hook 'jaspace-mode-off-hook
+;	    (lambda()
+;	      (when (boundp 'show-trailing-whitespace)
+;		(setq show-trailing-whitespace nil))))
+;  (add-hook 'jaspace-mode-hook
+;	    (lambda()
+;	      (progn
+;		(when (boundp 'show-trailing-whitespace)
+;		  (setq show-trailing-whitespace t))
+;		(face-spec-set 'jaspace-highlight-jaspace-face
+;			       '((((class color) (background light))
+;				  (:foreground "blue"))
+;				 (t (:foreground "green"))))
+;		(face-spec-set 'jaspace-highlight-tab-face
+;			       '((((class color) (background light))
+;				  (:foreground "red"
+;					       :background "unspecified"
+;					       :strike-through nil
+;					       :underline nil))
+;				 (t (:foreground "purple"
+;						 :background "unspecified"
+;						 :strike-through nil
+;						 :underline nil))))
+;		(face-spec-set 'trailing-whitespace
+;			       '((((class color) (background light))
+;				  (:foreground "red"
+;					       :background "unspecified"
+;					       :strike-through nil
+;					       :underline t))
+;				 (t (:foreground "purple"
+;						 :background "unspecified"
+;						 :strike-through nil
+;						 :underline t))))))))
 
-;; タブ, 全角スペース、改行直前の半角スペースを表示する
-(when (require 'jaspace nil t)
-  (when (boundp 'jaspace-modes)
-    (setq jaspace-modes (append jaspace-modes
-(list 'php-mode
-				      'yaml-mode
-				      'javascript-mode
-				      'ruby-mode
-				      'text-mode
-				      'fundamental-mode))))
-  (when (boundp 'jaspace-alternate-jaspace-string)
-    (setq jaspace-alternate-jaspace-string "□"))
-;  (when (boundp 'jaspace-highlight-tabs)
-;    (setq jaspace-highlight-tabs ?^))
-  (add-hook 'jaspace-mode-off-hook
-	    (lambda()
-	      (when (boundp 'show-trailing-whitespace)
-		(setq show-trailing-whitespace nil))))
-  (add-hook 'jaspace-mode-hook
-	    (lambda()
-	      (progn
-		(when (boundp 'show-trailing-whitespace)
-		  (setq show-trailing-whitespace t))
-		(face-spec-set 'jaspace-highlight-jaspace-face
-			       '((((class color) (background light))
-				  (:foreground "blue"))
-				 (t (:foreground "green"))))
-		(face-spec-set 'jaspace-highlight-tab-face
-			       '((((class color) (background light))
-				  (:foreground "red"
-					       :background "unspecified"
-					       :strike-through nil
-					       :underline nil))
-				 (t (:foreground "purple"
-						 :background "unspecified"
-						 :strike-through nil
-						 :underline nil))))
-		(face-spec-set 'trailing-whitespace
-			       '((((class color) (background light))
-				  (:foreground "red"
-					       :background "unspecified"
-					       :strike-through nil
-					       :underline t))
-				 (t (:foreground "purple"
-						 :background "unspecified"
-						 :strike-through nil
-						 :underline t))))))))
 
+;; Use monospace font for the sentence of Japanese and ASCII character both mixed up together
+;; http://blog.livedoor.jp/tek_nishi/archives/8590439.html
+
+(set-face-attribute 'default nil :family "Menlo" :height 140)
+(set-fontset-font (frame-parameter nil 'font)
+		  'japanese-jisx0208
+		  (font-spec :family "Hiragino Kaku Gothic ProN"))
+(add-to-list 'face-font-rescale-alist
+	     '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
