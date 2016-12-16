e45cce53da1e009d03aeec544ac446a6544ed7ab
uraw <uraw@urgy.net>
2016-12-17 02:38:20 +0900

    emacs: Add auto-generated code in init.el

--- emacs/init.el
+++ emacs/init.el
@@ -33,3 +33,17 @@
 ;;; Setting for init-loader.el
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (bind-key ruby-electric ruby-block popwin markdown-mode init-loader haml-mode fuzzy-format auto-install auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
