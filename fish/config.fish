dd66181b9fb4b28913cccec72eeb59ccf897203b
uraw <uraw@urgy.net>
2023-01-06 20:24:21 +0900

    fish: Add homebrew setting for linux

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,9 @@ if status is-interactive
     if test -e /opt/homebrew/bin/brew
         eval (/opt/homebrew/bin/brew shellenv)
     end
+    if test -e /home/linuxbrew/.linuxbrew/bin/brew
+        eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    end
 end
 
 
